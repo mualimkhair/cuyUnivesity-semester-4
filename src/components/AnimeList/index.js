03e204dd3e6cc90290e5ff1e8bd6ca05636aa627
mualimkhair <mualimkhair@gmail.com>
2025-05-26 17:54:34 +0800

    feat: create input search

--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -4,9 +4,9 @@ import Link from "next/link";
 const AnimeList = ({ api }) => {
   return (
     <div class="grid grid-cols-2 md:grid-cols-4 gap-2 px-4">
-      {api.data.map((anime) => {
+      {api.data.map((anime, index) => {
         return (
-          <Link href={`${anime.mal_id}`} className="cursor-pointer text-primary hover:text-accent transition-all">
+          <Link href={`${anime.mal_id}`} className="cursor-pointer text-primary hover:text-accent transition-all" key={index}>
             <Image src={anime.images.webp.image_url} alt="..." width={600} height={400} className="w-full max-h-72 object-cover" />
             <h3 className="font-bold lg:text-xl p-4 text-base ">{anime.title}</h3>
           </Link>
--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -11,10 +11,11 @@ const InputSearch = () => {
   const router = useRouter()
 
   const handleSearch = (event) => {
-
-    if (event.key === "Enter" || event.type === "click") {
-    event.preventDefault()
+    
     const keyword = searchRef.current.value
+    if (event.key === "Enter" || event.type === "click") {
+      
+    event.preventDefault()
     router.push(`/search/${keyword}`)
     }
 
